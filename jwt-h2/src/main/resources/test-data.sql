dbb4f48dc603659464de9ae81608f571e5790cfe
Eder Ortega <nwoswo@gmail.com>
2021-02-22 18:54:14 -0500

    Finish jwt-h2

--- jwt-h2/src/main/resources/test-data.sql
+++ jwt-h2/src/main/resources/test-data.sql
@@ -1,3 +1,4 @@
-INSERT INTO Usuario(id, username, password, roles, email ) VALUES (1, 'admin', '$2a$10$ucSwvKmvAZcuAHfgi3nNduEcmThs3C60LuxZTFK3v4Ijn/bfOybPW', 'ROLE_ADMIN,ROLE_USER', 'admin@test.com');
-INSERT INTO Usuario(id, username, password, roles, email ) VALUES (2, 'ruben', '$2a$10$ucSwvKmvAZcuAHfgi3nNduEcmThs3C60LuxZTFK3v4Ijn/bfOybPW', 'ROLE_USER', 'ruben@test.com');
+INSERT INTO Usuario(id, username, password, roles, email ) VALUES (1, 'system', '$2a$10$ucSwvKmvAZcuAHfgi3nNduEcmThs3C60LuxZTFK3v4Ijn/bfOybPW', 'ROLE_ADMIN,ROLE_USER', 'system@test.com');
+INSERT INTO Usuario(id, username, password, roles, email ) VALUES (2, 'admin', '$2a$10$ucSwvKmvAZcuAHfgi3nNduEcmThs3C60LuxZTFK3v4Ijn/bfOybPW', 'ROLE_ADMIN', 'admin@test.com');
+INSERT INTO Usuario(id, username, password, roles, email ) VALUES (3, 'ruben', '$2a$10$ucSwvKmvAZcuAHfgi3nNduEcmThs3C60LuxZTFK3v4Ijn/bfOybPW', 'ROLE_USER', 'ruben@test.com');
 
